e1aadc2eaa026dc138a00b05438d6eb0544c690f
Jonathan Amobi <amobijonathan2@gmail.com>
2023-10-25 19:01:25 +0100

    FEAT: added endpoint to allow authenticated users get their profile

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require("cors")
 const helmet = require("helmet")
 const morgan = require("morgan")
-require("dotenv").config()
+// require("dotenv").config()
 const globalErrorHandler = require("./error/error.controller")
 const AuthRouter = require("./auth/auth.route")
 const UserRouter = require("./user/user.route")
